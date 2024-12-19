ff69167bf5e576b81a177c78b1af06f346b18b14
sungmin <lsm1511@naver.com>
2024-12-19 23:46:28 +0900

    js

--- optiday_react/src/App.js
+++ optiday_react/src/App.js
@@ -1,11 +1,13 @@
 
-import './App.css';
 import OptidayApp from './components/OptidayApp';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <OptidayApp/>
+      </BrowserRouter>
     </div>
   );
 }
--- optiday_react/src/index.js
+++ optiday_react/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,4 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-reportWebVitals();
