f048cbf3ef11192198f8e4dfb5d1b9bfa1a526a2
sungmin <lsm1511@naver.com>
2025-01-04 21:20:17 +0900

    Repository

--- optiday/src/main/java/com/optiday_min/optiday/jpa/TodoRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/jpa/TodoRepository.java
@@ -1,5 +1,6 @@
-package com.optiday_min.optiday.Todo;
+package com.optiday_min.optiday.jpa;
 
+import com.optiday_min.optiday.Todo.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo,Integer> {
--- optiday/src/main/java/com/optiday_min/optiday/jpa/UserRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/jpa/UserRepository.java
@@ -1,5 +1,6 @@
-package com.optiday_min.optiday.User;
+package com.optiday_min.optiday.jpa;
 
+import com.optiday_min.optiday.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
