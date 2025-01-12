76c22f1559ad535fef9ef45e56b19d9a643f9bad
sungmin <lsm1511@naver.com>
2025-01-12 22:10:31 +0900

    DTO

--- optiday/src/main/java/com/optiday_min/optiday/Dto/SignUpRequest.java
+++ optiday/src/main/java/com/optiday_min/optiday/Dto/SignUpRequest.java
@@ -1,6 +1,6 @@
-package com.optiday_min.optiday.User.Dto;
+package com.optiday_min.optiday.Dto;
 
-import com.optiday_min.optiday.User.User;
+import com.optiday_min.optiday.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,19 +16,18 @@ public class SignUpRequest {
     private String email;
     private String phone;
 
-    private LocalDate birthday=null; //기본값
+    private LocalDate birthdate=null; //기본값
     private String message="";
 
     // User 엔티티 객체 생성
-    public User toEntity(){
-        return User.builder()
+    public Member toEntity(){
+        return Member.builder()
                 .name(this.name)
                 .password(this.password)
                 .message(this.message)
                 .email(this.email)
                 .phone(this.phone)
-                .birthday(this.birthday)
+                .birthdate(this.birthdate)
                 .build();
     }
-
 }
