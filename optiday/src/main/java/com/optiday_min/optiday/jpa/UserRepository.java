e378345aa5b34deff55a3a84f6f9f90434dd5002
sungmin <lsm1511@naver.com>
2025-01-12 22:10:19 +0900

    Update

--- optiday/src/main/java/com/optiday_min/optiday/jpa/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.optiday_min.optiday.jpa;
-
-import com.optiday_min.optiday.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
