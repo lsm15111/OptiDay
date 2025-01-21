0b59136c2efa60ee850769a92c7e096f0f87098a
sungmin <lsm1511@naver.com>
2025-01-21 21:01:14 +0900

    Member

--- optiday/src/main/java/com/optiday_min/optiday/controller/MemberController.java
+++ optiday/src/main/java/com/optiday_min/optiday/controller/MemberController.java
@@ -2,11 +2,14 @@ package com.optiday_min.optiday.controller;
 
 import com.optiday_min.optiday.Dto.MemberUpdateDto;
 import com.optiday_min.optiday.Dto.ProfileDto;
+import com.optiday_min.optiday.Dto.ProfileUpdateRequest;
 import com.optiday_min.optiday.Dto.SignUpRequestDto;
 import com.optiday_min.optiday.service.MemberService;
 import com.optiday_min.optiday.entity.Member;
 import com.optiday_min.optiday.jpa.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,7 +34,7 @@ public class MemberController {
 
 
     // 사용자 조회
-    @GetMapping("/member/{username}")
+    @GetMapping("/members/{username}")
     public Optional<Member> retrieveUser(@PathVariable String username) {
         Optional<Member> member = memberRepository.findByUsername(username);
         return member;
@@ -40,30 +43,32 @@ public class MemberController {
     
 
     // 사용자 생성 SignUpRequest DTO -> User Entity
-    @PostMapping("/member")
+    @PostMapping("/members")
     public ResponseEntity<Member> createUser(@RequestBody SignUpRequestDto signUpRequest) {
-        try{
-            Member member = memberService.registerMember(signUpRequest);
-            return ResponseEntity.ok(member);
-        }catch (IllegalArgumentException e){
-            return ResponseEntity.badRequest().build();
-        }
+        Member member = memberService.registerMember(signUpRequest);
+        return ResponseEntity.ok(member);
     }
 
     // 사용자 수정
-    @PutMapping("/member/{memberId}")
+    @PutMapping("/members/{memberId}")
     public void updateUser(@PathVariable Integer memberId, @RequestBody MemberUpdateDto memberUpdateDto) {
         memberService.toMember(memberId, memberUpdateDto);
     }
     // 사용자 삭제
-    @DeleteMapping("/member/{memberId}")
+    @DeleteMapping("/members/{memberId}")
     public void deleteUser(@PathVariable Integer memberId){
         memberRepository.deleteById(memberId);
     }
 
     // 사용자 프로필 조회
-    @GetMapping("/member/{username}/profile")
+    @GetMapping("/members/{username}/profile")
     public ProfileDto retrieveProfile(@PathVariable String username) {
         return memberService.getUserWithFollowCount(username);
+    }
+
+    @PutMapping("/members/{username}/profile")
+    public ResponseEntity<String> updateProfileForUsername(@PathVariable String username, @RequestBody ProfileUpdateRequest profileUpdateRequest) {
+        memberService.updateProfile(username, profileUpdateRequest);
+        return ResponseEntity.ok("Profile updated successfully");
     }
 }
