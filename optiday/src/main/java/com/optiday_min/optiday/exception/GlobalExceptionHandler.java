ee8e83d8490acac1594e3211cf5129ef48596ec1
sungmin <lsm1511@naver.com>
2025-02-02 22:53:32 +0900

    Exception

--- optiday/src/main/java/com/optiday_min/optiday/exception/GlobalExceptionHandler.java
+++ optiday/src/main/java/com/optiday_min/optiday/exception/GlobalExceptionHandler.java
@@ -15,27 +15,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 인수 예외
+    // Argument
+    // 400 Bed Request
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage()); // 409 Conflict
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-    // 테이블 예외
+    // Entity
+    // 404 Not Found
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage()); // 404 Not Found
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
-    // 예외
+    // Server
+    // 500 Internal Server Error
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
     }
 
+
 // JWT Exception
+
     // Token 예외
+    // 406 Not Acceptable
     @ExceptionHandler(NotAvailableRequestException.class)
     public ResponseEntity<String> handleNotAvailableRequestException(Exception ex) {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Token error :"+ex.getMessage());
+    }
+
+    // 401 인증되지않음
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<String> handleInvalidTokenException(InvalidTokenException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
 
---
+++ optiday/src/main/java/com/optiday_min/optiday/exception/NotAvailableRequestException.java
@@ -0,0 +1,6 @@
+package com.optiday_min.optiday.exception;
+
+public class NotAvailableRequestException extends RuntimeException{
+    public NotAvailableRequestException() {
+    }
+}
