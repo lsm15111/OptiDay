df1b3580c09ed400a90f4fc47b2382a1dd293510
sungmin <lsm1511@naver.com>
2025-02-05 02:19:14 +0900

    sql

--- optiday/src/main/resources/data.sql
+++ optiday/src/main/resources/data.sql
@@ -1,8 +1,27 @@
+-- 역할(Role) 데이터 추가
+INSERT INTO roles (name) VALUES
+    ('ROLE_ADMIN'),
+    ('ROLE_USER');
 
 -- Members
-INSERT INTO member (username, password, message, birthdate, email, phone) VALUES
-    ('min', 'min', 'Hello!', '1990-01-01', 'john.doe@example.com', '010-1234-5678'),
-    ('su', 'su', 'Hi there!', '1985-05-20', 'jane.smith@example.com', '010-5678-1234');
+INSERT INTO member (username, message, birthdate, phone) VALUES
+     ('min', 'Hello!', '1990-01-01', '010-1234-5678'),
+     ('su', 'Hi there!', '1985-05-20', '010-5678-1234');
+
+-- 사용자(User) 데이터 추가
+-- 비밀번호는 Bcrypt로 암호화한 값
+INSERT INTO users (email, password, member_id) VALUES
+    ('min@naver.com', '$2a$10$xSU78Fir4hS31pSvmPQNN.LRKG09R/3z1uaMyFiaE09FOo2c3fInm',1), -- "min"
+    ('su@naver.com', '$2a$10$TLqeQY1WO4sptc08YphLbu7w/rPZjqIKT6dpvP4exJYl8l9XTimha',2); -- "su"
+
+-- 사용자와 역할 관계 추가 (user_roles 테이블)
+INSERT INTO user_roles (user_id, role_id) VALUES
+    (1, 1), -- min -> ROLE_ADMIN
+    (2, 2); -- su -> ROLE_USER
+
+
+
+
 
 -- Categories
 INSERT INTO category (name, color, member_id) VALUES
@@ -22,20 +41,28 @@ INSERT INTO category (name, color, member_id) VALUES
 INSERT INTO todo (title, description, start_date, end_date, status, member_id, category_id) VALUES
     ('연말 회의', '연말을 맞아 진행되는 회의입니다.', '2024-12-28', '2024-12-29', false, 1, 1),
     ('송년회', '올해의 마지막 송년회입니다.', '2024-12-30', '2024-12-31', false, 1, 3),
-    ('회의', '새해 첫 회의입니다.', '2025-01-01', '2025-01-03', false, 1, NULL),
+    ('회의', '새해 첫 회의입니다.', '2025-01-01', '2025-01-03', false, 1, 11),
     ('달리기', '신년 달리기 행사입니다.', '2025-01-04', '2025-01-05', false, 1, NULL),
     ('회의', '팀 회의입니다.', '2025-01-07', '2025-01-08', false, 1, 4),
     ('회의', '중요한 회의입니다.', '2025-01-01', '2025-01-03', false, 1, 2),
     ('개발 회의', '개발 팀 회의입니다.', '2025-01-09', '2025-01-10', false, 1, NULL),
-    ('디자인 검토', '디자인 검토 회의입니다.', '2025-01-11', '2025-01-12', false, 1, 1),
+    ('디자인 검토', '디자인 검토 회의입니다.', '2025-01-11', '2025-01-12', false, 1, 6),
     ('팀 빌딩', '팀 빌딩 활동입니다.', '2025-01-13', '2025-01-14', false, 1, 3),
     ('고객 미팅', '고객과의 미팅입니다.', '2025-01-15', '2025-01-16', false, 1, 4),
     ('프로젝트 계획', '프로젝트 계획 회의입니다.', '2025-01-17', '2025-01-18', false, 1, NULL),
-    ('테스트', '프로젝트 테스트 기간입니다.', '2025-01-10', '2025-01-20', false, 1, 1),
+    ('테스트', '프로젝트 테스트 기간입니다.', '2025-01-10', '2025-01-20', false, 1, 7),
     ('여행 계획', '주변 찾아보기', '2025-01-21', '2025-01-22', false, 1, 1),
-    ('회고', '팀 회고 회의입니다.', '2025-01-23', '2025-01-24', false, 1, 1),
+    ('회고', '팀 회고 회의입니다.', '2025-01-23', '2025-01-24', false, 1, 8),
     ('전략 회의', '전략 회의입니다.', '2025-01-25', '2025-01-26', false, 1, 4),
-    ('교육 세션', '신입 교육 세션입니다.', '2025-01-27', '2025-01-28', false, 1, 1),
+    ('교육 세션', '신입 교육 세션입니다.', '2025-01-27', '2025-01-28', false, 1, 10),
     ('신년 계획', '신년 계획 수립 회의입니다.', '2025-02-01', '2025-02-02', false, 1, 1),
     ('마케팅 회의', '마케팅 전략 회의입니다.', '2025-02-05', '2025-02-06', false, 1, NULL);
+
+-- -- Follow
+-- INSERT INTO follow ( follower_id, following_id, created_at) VALUES
+--     (1,2,'2025-01-01T10:00:00'),
+--     (2,1,'2025-01-01T10:00:00'),
+--     (3,1,'2025-01-01T10:00:00'),
+--     (4,1,'2025-01-01T10:00:00');
+--
 
