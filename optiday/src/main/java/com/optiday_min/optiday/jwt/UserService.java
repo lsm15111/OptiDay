550b33f052ca31e24e7752fee25e581cc521ab1d
sungmin <lsm1511@naver.com>
2025-02-05 02:17:03 +0900

    token -> Email 반환 추가 회원 등록 Email 검증 추가

--- optiday/src/main/java/com/optiday_min/optiday/jwt/UserService.java
+++ optiday/src/main/java/com/optiday_min/optiday/jwt/UserService.java
@@ -5,32 +5,37 @@ import com.optiday_min.optiday.exception.NotAvailableRequestException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenUtil jwtTokenUtil;
 
-    @Value("${jwt.secret.key}")
-    private String SECRET_KEY;
 
     // 회원 권한 등록
     public User registerUser(String email, String password,Member member) {
+
+        if(userRepository.existsByEmail(email)){
+            logger.warn("User with email {} already exists", email);
+            throw new NotAvailableRequestException();
+        }
+
         User user = new User();
-        user.setUsername(email);
+        user.setEmail(email);
         user.setPassword(passwordEncoder.encode(password));
         user.setMember(member);
 
@@ -39,16 +44,25 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("Role not found"));
         user.setRoles(Collections.singleton(userRole));
 
+        logger.info("Registering user " + user.getEmail());
         return userRepository.save(user);
     }
 
+    public String getEmailByMemberId(Long memberId) {
+        User user = userRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getEmail();
+    }
+
     public boolean authenticateUser(JwtTokenRequest request) {
-        User user = userRepository.findByUsername(request.getEmail())
+        User user = userRepository.findByEmail(request.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         // 복호화 == 암호화 비교
         if(passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            logger.info("Authenticated user " + user.getEmail());
             return true;
         }
+        logger.warn("Invalid username or password");
         return false;
     }
 
@@ -58,16 +72,16 @@ public class UserService {
             // 토큰 추출
             token = jwtTokenUtil.extractToken(token);
             // JWT 파싱 및 검증
-            Claims claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
-                    .parseClaimsJws(token)
-                    .getBody();
-            System.out.println("JWT 유효함! 사용자: " + claims.getSubject());
-            Optional<User> user = userRepository.findByUsername(claims.getSubject());
-
-            return user.get().getMember().getId();
+            Long memberId = jwtTokenUtil.getMemberId(token);
+            logger.info("JWT Valid! MemberId : " + memberId);
+            return memberId;
         } catch (Exception e) {
+            logger.warn("Invalid token");
             throw new NotAvailableRequestException();
         }
+    }
+
+    public boolean isEmail(String email) {
+        return userRepository.existsByEmail(email);
     }
 }
