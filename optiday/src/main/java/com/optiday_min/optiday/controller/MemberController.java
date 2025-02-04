33a6d0ce039f6a4d3706e734fd5cba2c3e3f306c
sungmin <lsm1511@naver.com>
2025-02-05 02:15:46 +0900

    token auth 적용 AccountSearch 페이징적용

--- optiday/src/main/java/com/optiday_min/optiday/controller/MemberController.java
+++ optiday/src/main/java/com/optiday_min/optiday/controller/MemberController.java
@@ -1,74 +1,160 @@
 package com.optiday_min.optiday.controller;
 
-import com.optiday_min.optiday.Dto.MemberUpdateDto;
-import com.optiday_min.optiday.Dto.ProfileDto;
-import com.optiday_min.optiday.Dto.ProfileUpdateRequest;
-import com.optiday_min.optiday.Dto.SignUpRequestDto;
+import com.optiday_min.optiday.dto.*;
+import com.optiday_min.optiday.exception.NotAvailableRequestException;
+import com.optiday_min.optiday.jwt.JwtTokenUtil;
+import com.optiday_min.optiday.jwt.UserRepository;
+import com.optiday_min.optiday.jwt.UserService;
 import com.optiday_min.optiday.service.MemberService;
 import com.optiday_min.optiday.entity.Member;
 import com.optiday_min.optiday.jpa.MemberRepository;
-import jakarta.persistence.EntityNotFoundException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.hibernate.tool.schema.spi.DelayedDropRegistryNotAvailableImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+import static javax.crypto.Cipher.SECRET_KEY;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
     private final MemberRepository memberRepository;
+    private final UserRepository userRepository;
 
-    
+    private final UserService userService;
+
+
     //TODO : 관리자 전용 설정하기
     // 사용자 모두 출력
-    @GetMapping("/members")
-    public List<Member> user() {
-        return memberRepository.findAll();
-    }
-
-
-    // 사용자 조회
-    @GetMapping("/members/{username}")
-    public Optional<Member> retrieveUser(@PathVariable String username) {
-        Optional<Member> member = memberRepository.findByUsername(username);
+    @GetMapping("/all")
+    public List<Member> member() {
+        List<Member> member = memberRepository.findAll();
         return member;
     }
 
-    
+    // 회원 조회
+    @GetMapping("/me")
+    public ResponseEntity<Member> retrieveMember(@RequestHeader("Authorization") String token) {
 
-    // 사용자 생성 SignUpRequest DTO -> User Entity
-    @PostMapping("/members")
-    public ResponseEntity<Member> createUser(@RequestBody SignUpRequestDto signUpRequest) {
-        Member member = memberService.registerMember(signUpRequest);
+        Long memberId = userService.getMemberIdForToken(token);
+        Member member = memberService.getMemberIdForMember(memberId);
         return ResponseEntity.ok(member);
     }
+    // 사용자 생성 SignUpRequest DTO -> User Entity
+    @PostMapping("/signup")
+    public ResponseEntity<String> createUser(@Valid @RequestBody SignUpRequestDto signUpRequest) {
 
-    // 사용자 수정
-    @PutMapping("/members/{memberId}")
-    public void updateUser(@PathVariable Integer memberId, @RequestBody MemberUpdateDto memberUpdateDto) {
-        memberService.toMember(memberId, memberUpdateDto);
-    }
-    // 사용자 삭제
-    @DeleteMapping("/members/{memberId}")
-    public void deleteUser(@PathVariable Integer memberId){
-        memberRepository.deleteById(memberId);
-    }
-
-    // 사용자 프로필 조회
-    @GetMapping("/members/{username}/profile")
-    public ProfileDto retrieveProfile(@PathVariable String username) {
-        return memberService.getUserWithFollowCount(username);
+        // Member 생성
+        Member member = memberService.registerMember(signUpRequest);
+        // User 등록
+        userService.registerUser(signUpRequest.getEmail(), signUpRequest.getPassword(),member);
+        return ResponseEntity.ok("Member created successfully");
     }
 
-    @PutMapping("/members/{username}/profile")
-    public ResponseEntity<String> updateProfileForUsername(@PathVariable String username, @RequestBody ProfileUpdateRequest profileUpdateRequest) {
-        memberService.updateProfile(username, profileUpdateRequest);
+    @GetMapping("/message")
+    public String retrieveMessage(@RequestHeader("Authorization") String token) {
+        Long memberId = userService.getMemberIdForToken(token);
+        Member member = memberService.getMemberIdForMember(memberId);
+        return  member.getMessage();
+    }
+
+
+
+
+
+//
+//    // 사용자 생성 SignUpRequest DTO -> User Entity
+//    @PostMapping("/members")
+//    public ResponseEntity<Member> createUser(@RequestHeader("Authorization") String token,
+//                                             @RequestBody SignUpRequestDto signUpRequest) {
+//        // 요청 예외처리 (DTO 내부 값이 null로 들어왔을때)
+//
+//        try{
+//            if(signUpRequest.getUsername()!=null&&signUpRequest.getUsername().length()>=2){
+//
+//            }else if (signUpRequest.getPassword()!=null) {
+//
+//            }
+//
+//
+//        }catch (Exception e){
+//            throw new IllegalArgumentException();
+//        }
+//        //throw new NotAvailableRequestException();
+//
+//        Member member = memberService.registerMember(signUpRequest);
+//        return ResponseEntity.ok(member);
+//    }
+//
+//    // 사용자 수정 -            멱등성
+//    @PutMapping("/members/{memberId}")
+//    public void updateUser(@RequestHeader("Authorization") String token,
+//                           @RequestBody MemberUpdateDto memberUpdateDto) {
+//        memberService.updateMember(memberId, memberUpdateDto);
+//    }
+//    // 사용자 삭제
+//    @DeleteMapping("/members/{memberId}")
+//    public void deleteUser(@RequestHeader("Authorization") String token){
+//        memberRepository.deleteById(memberId);
+//    }
+//
+    // 내 프로필 조회
+    @GetMapping("/profile")
+    public ResponseEntity<ProfileDto> retrieveProfile(@RequestHeader("Authorization") String token) {
+        Long memberId = userService.getMemberIdForToken(token);
+        ProfileDto profile = memberService.getUserWithFollowCount(memberId);
+        return ResponseEntity.ok(profile);
+    }
+//
+    // 프로필 업데이트
+    @PutMapping("/profile")
+    public ResponseEntity<String> updateProfile(@RequestHeader("Authorization") String token,
+                                                           @RequestBody ProfileUpdate profileUpdate) {
+        Long memberId = userService.getMemberIdForToken(token);
+        memberService.updateProfile(memberId, profileUpdate);
         return ResponseEntity.ok("Profile updated successfully");
+    }
+//    //상대방의 profile 보기
+//    @GetMapping("/account/search/{pickMemberId}")
+//    public MemberProfile retrieveProfileForMemberId(@RequestHeader("Authorization") String token,
+//                                                    @PathVariable Integer pickMemberId) {
+//        return memberService.getMemberProfile(username,pickMemberId);
+//    }
+
+
+
+
+
+
+
+
+
+
+    // 멤버 데이터 반환 (Follow Search)
+    @GetMapping("/search")
+    public Page<?> getMembers(
+            @RequestHeader("Authorization") String token,
+            @RequestParam(value = "currentPage", defaultValue = "0") int currentPage,
+            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+            @RequestParam(value = "search", defaultValue = "") String search // 검색어 추가
+    ) {
+        Long memberId = userService.getMemberIdForToken(token);
+//        int batchSize = 10; // 10페이지 묶음 단위
+        int startPage = currentPage * pageSize; //시작 페이지 계산
+
+        // 100개단위 페이지 반환
+        // (시작크기,시작+size 까지,검색어)
+        return memberService.searchMembers(startPage, pageSize, search,memberId);
     }
 }
