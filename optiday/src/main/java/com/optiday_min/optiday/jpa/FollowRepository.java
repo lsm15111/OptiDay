9e7cb679ea6ec720c6054f9ee538293cd272479c
sungmin <lsm1511@naver.com>
2025-02-06 19:47:46 +0900

    N+1문제, FETCH JOIN 사용

--- optiday/src/main/java/com/optiday_min/optiday/jpa/FollowRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/jpa/FollowRepository.java
@@ -3,18 +3,21 @@ package com.optiday_min.optiday.jpa;
 import com.optiday_min.optiday.entity.Follow;
 import com.optiday_min.optiday.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface FollowRepository extends JpaRepository<Follow, Integer> {
+public interface FollowRepository extends JpaRepository<Follow, Long> {
     boolean existsByFollowerAndFollowing(Member follower, Member following);
+
+    boolean existsByFollowerIdAndFollowingId(Long followerId, Long followingId);
 
     void deleteByFollowerAndFollowing(Member follower, Member following);
 
-    List<Follow> findAllByFollower(Member follower); //내가 팔로우한 사람들
-
-    List<Follow> findAllByFollowing(Member following); //나를 팔로우한 사람들
-
+    @Query("SELECT f FROM Follow f JOIN FETCH f.follower f1 JOIN FETCH f.following f2 WHERE f1.id = :memberId OR f2.id = :memberId")
+    List<Follow> findFollowingsAndFollowersWithFetch(@Param("memberId") Long memberId);
 }
+
