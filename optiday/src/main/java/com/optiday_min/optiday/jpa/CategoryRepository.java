d456bdca668e1c53037e22eed31833eeb6bc26b7
sungmin <lsm1511@naver.com>
2025-02-06 19:48:07 +0900

    CategoryRepository 추가

--- optiday/src/main/java/com/optiday_min/optiday/jpa/CategoryRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/jpa/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.optiday_min.optiday.jpa;
 import com.optiday_min.optiday.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByMemberId(Long memberId);
 }
