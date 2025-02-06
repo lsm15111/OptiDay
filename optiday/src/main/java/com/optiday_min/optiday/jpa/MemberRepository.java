71d1877eade4dcf70ee9d1393bca76062c870889
sungmin <lsm1511@naver.com>
2025-02-06 19:46:59 +0900

    페이징 추가

--- optiday/src/main/java/com/optiday_min/optiday/jpa/MemberRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/jpa/MemberRepository.java
@@ -1,11 +1,21 @@
 package com.optiday_min.optiday.jpa;
 
 import com.optiday_min.optiday.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
-    boolean existsByUsername(String username); // 이름 중복 체크
+    boolean existsByUsername(String username);
+
+    //페이징
+    Page<Member> findByIdNot(Long myId, Pageable pageable);
+    Page<Member> findByUsernameContainingAndIdNot(String search, Long myId, Pageable pageable);
+    Page<Member> findByUsernameContaining(String username, Pageable pageable);
+
+//    List<AccountSearchDto> findAllBy();
+
 }
