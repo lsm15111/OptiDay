ba6a46183a2c3ca71ac3a7b9ae01575ded0c0194
sungmin <lsm1511@naver.com>
2025-02-10 20:44:33 +0900

    UserService

--- optiday/src/main/java/com/optiday_min/optiday/jwt/UserService.java
+++ optiday/src/main/java/com/optiday_min/optiday/jwt/UserService.java
@@ -1,13 +1,12 @@
 package com.optiday_min.optiday.jwt;
 
 import com.optiday_min.optiday.dto.AccountDeleteRequest;
-import com.optiday_min.optiday.entity.Member;
+import com.optiday_min.optiday.domain.Member;
 import com.optiday_min.optiday.exception.NotAvailableRequestException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
