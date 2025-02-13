89d39cd32988a52dc36fd37ab411efe93fa046f1
sungmin <lsm1511@naver.com>
2025-02-13 21:04:12 +0900

    Dto

--- optiday/src/main/java/com/optiday_min/optiday/dto/AccountDeleteRequest.java
+++ optiday/src/main/java/com/optiday_min/optiday/dto/AccountDeleteRequest.java
@@ -1,11 +1,15 @@
 package com.optiday_min.optiday.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @Getter
 @Setter
 public class AccountDeleteRequest {
+    @NotBlank(message = "이메일은 필수 입력값 입니다")
+    @Email(message = "이메일 형식이어야 합니다")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력값 입니다")
     private String password;
 }
--- optiday/src/main/java/com/optiday_min/optiday/dto/AccountSearchResponse.java
+++ optiday/src/main/java/com/optiday_min/optiday/dto/AccountSearchResponse.java
@@ -1,9 +1,6 @@
 package com.optiday_min.optiday.dto;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
