ce7842fdca6456769e455babe4a527d390b54e4e
sungmin <lsm1511@naver.com>
2025-02-15 23:48:20 +0900

    C,U,D 추가

--- optiday/src/main/java/com/optiday_min/optiday/controller/CategoryController.java
+++ optiday/src/main/java/com/optiday_min/optiday/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.optiday_min.optiday.controller;
 
-import com.optiday_min.optiday.entity.Category;
+import com.optiday_min.optiday.dto.CategoryRequest;
+import com.optiday_min.optiday.domain.Category;
+import com.optiday_min.optiday.dto.CategoryResponse;
+import com.optiday_min.optiday.repository.CategoryRepository;
+import com.optiday_min.optiday.jwt.UserService;
 import com.optiday_min.optiday.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +15,51 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+    private final UserService userService;
 
-    @GetMapping("/categories") //모든 카테고리 조회
+    @GetMapping("/all") //모든 카테고리 조회
     public List<Category> retrieveAllCategories() {
         return categoryService.getAllCategories();
     }
 
-    @GetMapping("/members/{username}/categories")
-    public ResponseEntity<List<Category>> retrieveAllCategoriesByUsername(@PathVariable String username) {
-        List<Category> categories = categoryService.getCategoryByUsername(username);
+    // 조회
+    @GetMapping("/me")
+    public ResponseEntity<List<Category>> retrieveAllCategoriesByEmail(@RequestHeader("Authorization") String token) {
+        Long memberId = userService.getMemberIdForToken(token);
+        List<Category> categories = categoryService.getCategoryByMemberId(memberId);
         if (categories.isEmpty()) {
             return ResponseEntity.noContent().build(); // 204 No Content
         }
         return ResponseEntity.ok(categories); // 200 OK
     }
 
-//    @PostMapping("/members/{username}/")
+    // 생성
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestHeader("Authorization") String token,
+                                                   @Valid @RequestBody CategoryRequest categoryRequest) {
+        Long memberId = userService.getMemberIdForToken(token);
+        Category category = categoryService.saveCategory(categoryRequest,memberId);
+        return ResponseEntity.ok(category);
+    }
+    
+    // 수정
+    @PutMapping("{categoryId}")
+    public ResponseEntity<Category> updateCategory(@RequestHeader("Authorization") String token,
+                                                   @Valid @RequestBody CategoryRequest categoryRequest,
+                                                   @PathVariable Long categoryId) {
+        Long memberId = userService.getMemberIdForToken(token);
+        Category category = categoryService.updateCategory(categoryRequest,memberId,categoryId);
+        return ResponseEntity.ok(category);
+    }
+
+    // 삭제
+    @DeleteMapping("{categoryId}")
+    public void deleteCategory(@RequestHeader("Authorization") String token, @PathVariable Long categoryId) {
+        Long memberId = userService.getMemberIdForToken(token);
+        categoryService.removeCategory(memberId,categoryId);
+    }
+
 }
--- optiday/src/main/java/com/optiday_min/optiday/service/CategoryService.java
+++ optiday/src/main/java/com/optiday_min/optiday/service/CategoryService.java
@@ -1,28 +1,66 @@
 package com.optiday_min.optiday.service;
 
-import com.optiday_min.optiday.entity.Category;
-import com.optiday_min.optiday.entity.Member;
-import com.optiday_min.optiday.jpa.CategoryRepository;
-import com.optiday_min.optiday.jpa.MemberRepository;
+import com.optiday_min.optiday.dto.CategoryRequest;
+import com.optiday_min.optiday.domain.Category;
+import com.optiday_min.optiday.domain.Member;
+import com.optiday_min.optiday.dto.CategoryResponse;
+import com.optiday_min.optiday.exception.NotAvailableRequestException;
+import com.optiday_min.optiday.repository.CategoryRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
-    private final CategoryRepository categoryRepository;
-    private final MemberRepository memberRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final CategoryRepository categoryRepository;
+    private final TodoService todoService;
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
-    public List<Category> getCategoryByUsername(String username) {
-        return memberRepository.findByUsername(username)
-                .map(Member::getCategories)
-                .orElseThrow(() -> new UsernameNotFoundException("Member not found with username: " + username));
+    public List<Category> getCategoryByMemberId(Long memberId) {
+        List<Category> categories = categoryRepository.findByMemberId(memberId);
+        return categories;
+    }
+
+    public Category saveCategory(CategoryRequest categoryRequest, Long memberId) {
+        Member memberRef = entityManager.getReference(Member.class, memberId);
+
+        Category category = new Category(categoryRequest.getName(), categoryRequest.getColor()
+                                        ,memberRef);
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(CategoryRequest categoryRequest, Long memberId, Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new EntityNotFoundException("Category not found"));
+
+        // token - category 소유 체크
+        if(!Objects.equals(category.getMember().getId(), memberId)){
+            throw new NotAvailableRequestException();
+        }
+        category.setName(categoryRequest.getName());
+        category.setColor(categoryRequest.getColor());
+        categoryRepository.save(category);
+        return category;
+    }
+
+    public void removeCategory(Long memberId,Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new UsernameNotFoundException("Category not found"));
+        // 카테고리에 속한 todos 가져와서 category=null 설정
+        todoService.removeCategoryFromTodos(category);
+        categoryRepository.deleteById(categoryId);
     }
 }
