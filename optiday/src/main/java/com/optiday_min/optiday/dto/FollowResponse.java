a047857151a16aa4b2ab38dc39479d40a410aae2
sungmin <lsm1511@naver.com>
2025-02-15 23:46:48 +0900

    Dto

--- optiday/src/main/java/com/optiday_min/optiday/dto/FollowResponse.java
+++ optiday/src/main/java/com/optiday_min/optiday/dto/FollowResponse.java
@@ -1,9 +1,6 @@
 package com.optiday_min.optiday.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
