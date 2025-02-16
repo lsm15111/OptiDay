7792878515bad3993deca3de5b7ebdbf9c1a826f
sungmin <lsm1511@naver.com>
2025-02-17 01:18:21 +0900

    카테고리 아이콘 추가

--- optiday_react/src/components/Categorylist.jsx
+++ optiday_react/src/components/Categorylist.jsx
@@ -4,11 +4,13 @@ import "../styles/Categorylist.css";
 import { CirclePicker } from "react-color";
 import { useEffect, useRef, useState } from "react";
 import { updateCategoryColor } from "../redux/slices/categorySlice";
+import { Plus, PlusCircle, PlusSquare, Trash, Trash2 } from "lucide-react";
 function Categorylist(){
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories.categories);
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
   const [showColorPicker, setShowColorPicker] = useState(false);
+  const [originalColor, setOriginalColor] = useState(null);
   const pickerRef = useRef(null);
 
   // 12가지 색상 목록
@@ -18,16 +20,17 @@ function Categorylist(){
   ];
 
   // 카테고리 선택 핸들러
-  const handleCategorySelect = (id) => {
+  const handleCategorySelect = (id,color) => {
     setSelectedCategoryId(id);
     setShowColorPicker(true);
+    setOriginalColor(color);
   };
 
   // 색상 변경 핸들러 (바로 Redux에 반영)
   const handleColorChange = (color) => {
     if (selectedCategoryId) {
+      
       dispatch(updateCategoryColor({ id: selectedCategoryId, color: color.hex }));
-      setShowColorPicker(false); // 색상 선택 후 피커 닫기
     }
   };
 
@@ -36,6 +39,7 @@ function Categorylist(){
     const handleClickOutside = (event) => {
       if (pickerRef.current && !pickerRef.current.contains(event.target)) {
         setShowColorPicker(false);
+
       }
     };
 
@@ -44,16 +48,17 @@ function Categorylist(){
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
- // 선택된 카테고리의 색상 가져오기
+//  선택된 카테고리의 색상 가져오기
  const selectedCategory = categories.find((cat) => cat.id === selectedCategoryId);
 
     return (
       <div className="main-categorylist">
-        <h2>카테고리 색상 선택</h2>
+        <h4>나의 카테고리 설정</h4>
+        <PlusCircle size={35} className="plus-square"/>
         {categories.map((cat) => (
           <div key={cat.id} style={{ display: 'inline-block' }}>
             <button
-              onClick={() => handleCategorySelect(cat.id)}
+              onClick={() => handleCategorySelect(cat.id,cat.color)}
               className="categorylist-select-button"
               style={{ backgroundColor: cat.color }}
             >
@@ -70,6 +75,9 @@ function Categorylist(){
                   colors={colors}
                   onChangeComplete={handleColorChange}
                 />
+                <div className="categorylist-picker-button">
+                  <Trash2 size={25} className="categorylist-delete"/>
+                </div>
               </div>
             )}
           </div>
