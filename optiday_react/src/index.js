4fc18fe374122bd4f27f63dab7b175c807dfa5d6
sungmin <lsm1511@naver.com>
2025-02-17 01:22:07 +0900

    React.StrictMode 개발모드에서 비활성화

--- optiday_react/src/index.js
+++ optiday_react/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
-
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> {/*배포시 두번처리안됨. 주석하기*/}
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
+  // </React.StrictMode>
 );
