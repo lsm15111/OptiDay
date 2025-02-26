bf85b636108fb3b0c2362dc68c7143531012c85b
sungmin <lsm1511@naver.com>
2025-02-26 18:49:04 +0900

    username, isOwner 추가

--- optiday/src/main/java/com/optiday_min/optiday/dto/CommentResponse.java
+++ optiday/src/main/java/com/optiday_min/optiday/dto/CommentResponse.java
@@ -3,14 +3,20 @@ package com.optiday_min.optiday.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 
+@Getter
+@Setter
 @AllArgsConstructor
 @Builder
 public class CommentResponse {
     private Long id;
+    private String username;
     private String comment;
+    private boolean isOwner;
     private LocalDateTime createdAt;
 }
