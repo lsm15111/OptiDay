d8a118930fb4b4b4e73f6858553ff4dc3e32af41
sungmin <lsm1511@naver.com>
2025-02-26 18:54:29 +0900

    Fetch Join 적용

--- optiday/src/main/java/com/optiday_min/optiday/repository/CommentRepository.java
+++ optiday/src/main/java/com/optiday_min/optiday/repository/CommentRepository.java
@@ -2,7 +2,14 @@ package com.optiday_min.optiday.repository;
 
 import com.optiday_min.optiday.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByTodoId(Long todoId);
 
+    @Query("SELECT c FROM Comment c JOIN FETCH c.member WHERE c.todo.id = :todoId")
+    List<Comment> findByTodoIdWithMember(@Param("todoId") Long todoId);
 }
