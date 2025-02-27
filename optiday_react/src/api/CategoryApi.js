5eaa60958405e7bbd1c9298d347430a3a3bcedd1
sungmin <lsm1511@naver.com>
2025-02-27 22:00:16 +0900

    Patch API 추가

--- optiday_react/src/api/CategoryApi.js
+++ optiday_react/src/api/CategoryApi.js
@@ -4,10 +4,16 @@ export const retrieveCategoriesApi
     = () => apiClient.get(`/categories/me`)
 
 export const createCategoryApi
-    = (category) => apiClient.post(`/categoryies`, category)
+    = (category) => apiClient.post(`/categories`, category)
 
 export const updateCategoryApi
     = (category) => apiClient.put(`/categories/${category.id}`, category)
 
+export const updateCategoryNameApi
+    = (id,name) => apiClient.patch(`/categories/${id}/update-name`,{name:name})
+    
+export const updateCategoryColorApi
+    = (id,color) => apiClient.patch(`/categories/${id}/update-color`,{color:color})
+
 export const deleteCategoryApi
-    = (categoryId) => apiClient.delete(`/categoryies/${categoryId}`)
+    = (categoryId) => apiClient.delete(`/categories/${categoryId}`)
