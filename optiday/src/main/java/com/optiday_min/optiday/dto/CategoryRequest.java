fb090608d6bbaa339f22d08c6631db0b2b138bee
sungmin <lsm1511@naver.com>
2025-02-27 21:56:25 +0900

    size 제약 추가

--- optiday/src/main/java/com/optiday_min/optiday/dto/CategoryRequest.java
+++ optiday/src/main/java/com/optiday_min/optiday/dto/CategoryRequest.java
@@ -1,6 +1,7 @@
 package com.optiday_min.optiday.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 public class CategoryRequest {
     @NotBlank(message = "카테고리 이름은 필수 입력값 입니다")
+    @Size(max = 5,message = "카테고리 이름은 최대 5자 입니다")
     private String name;
     @NotBlank(message = "카테고리 색상은 필수 선택값 입니다")
     private String color;
