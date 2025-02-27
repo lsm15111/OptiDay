7d922050e27a05b0e61256fd202cc642dc15e3eb
sungmin <lsm1511@naver.com>
2025-02-27 21:59:08 +0900

    PATCH 허용

--- optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
+++ optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
                 .allowedOrigins("http://localhost:3000") // React 클라이언트 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 사용 허용 (필요 시)
     }
