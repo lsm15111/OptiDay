621a6be3a2ddfadba41d0ba46c54218533ab206f
sungmin <lsm1511@naver.com>
2025-03-07 16:31:18 +0900

    CommentController

--- optiday/src/main/java/com/optiday_min/optiday/controller/CommentController.java
+++ optiday/src/main/java/com/optiday_min/optiday/controller/CommentController.java
@@ -1,22 +1,34 @@
 package com.optiday_min.optiday.controller;
 
+import com.optiday_min.optiday.domain.Comment;
+import com.optiday_min.optiday.domain.Member;
+import com.optiday_min.optiday.dto.CommentRequest;
 import com.optiday_min.optiday.dto.CommentResponse;
+import com.optiday_min.optiday.jwt.UserService;
 import com.optiday_min.optiday.service.CommentService;
+import com.optiday_min.optiday.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/comments")
+@RequestMapping("/api//comments")
 @RequiredArgsConstructor
 public class CommentController {
 
+    private final UserService userService;
     private final CommentService commentService;
+    private final MemberService memberService;
 
 
-//    @GetMapping
-//    public List<CommentResponse>
+
+
+//    // Todos -> Comment List
+//    @GetMapping("{todoId}")
+//    public List<CommentResponse> retrieveCommentForTodoId(@PathVariable Long todoId){
+//
+//    }
 }
