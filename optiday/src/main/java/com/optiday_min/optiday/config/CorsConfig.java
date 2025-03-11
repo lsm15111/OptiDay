7f34003a8c14b4dffa1799e70df278121f3cf329
sungmin <lsm1511@naver.com>
2025-03-11 21:05:38 +0900

    테스트용

--- optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
+++ optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000") // React 클라이언트 주소
+                .allowedOrigins("*") // React 클라이언트 주소 "http://localhost:3000"
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 사용 허용 (필요 시)
+                .allowCredentials(false); // 쿠키 사용 허용 (필요 시) true
     }
 
 }
