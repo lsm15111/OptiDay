3c41e8d660bc4a72168ce202d82c5b0c78da43e3
sungmin <lsm1511@naver.com>
2025-03-14 18:24:04 +0900

    React CICD 추가, api 변경

--- optiday_react/src/api/ApiClient.js
+++ optiday_react/src/api/ApiClient.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const apiClient = axios.create(
     {
         // baseURL: 'http://localhost:8080/api' // 클라우드
-        baseURL: 'http://13.209.180.83/api' // 배포용
+        // baseURL: 'http://13.209.180.83/api' // 배포용
+        baseURL: process.env.REACT_APP_API_BASE_URL || "/api"
     }
 )
