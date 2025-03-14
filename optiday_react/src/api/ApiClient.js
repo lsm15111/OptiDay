b87b77817620477fcfa6a47067fdd30185e83642
sungmin <lsm1511@naver.com>
2025-03-14 19:17:56 +0900

    CICD

--- optiday_react/src/api/ApiClient.js
+++ optiday_react/src/api/ApiClient.js
@@ -5,6 +5,5 @@ export const apiClient = axios.create(
         // baseURL: 'http://localhost:8080/api' // 클라우드
         baseURL: 'http://13.209.180.83/api' // 배포용
         // baseURL: process.env.REACT_APP_API_BASE_URL || "/api"
-
     }
 )
