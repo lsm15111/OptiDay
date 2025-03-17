2b4cb827d945adafe393285679f149aedc408884
sungmin <lsm1511@naver.com>
2025-03-17 13:26:58 +0900

    배포 링크 추가

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # OptiDay
-## 개발 기간 2024.12 ~
+## 개발 기간 2024.12~2025.03
 
 노션 링크 : https://square-armchair-f1f.notion.site/OptiDay-1381669ed05c804c87c9fd25b3e66bf7?pvs=4
 
--- optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
+++ optiday/src/main/java/com/optiday_min/optiday/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.optiday_min.optiday.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${server.env}")
+    private String env;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String Origins = "";
+        if(env.equals("local")){
+            Origins = "http://localhost:3000";
+        }else{
+            Origins = "http://13.209.180.83";
+        }
+
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-//                .allowedOrigins("*") // React 클라이언트 주소 "http://localhost:3000"
-                .allowedOriginPatterns("*") //테스트용
+                .allowedOrigins(Origins) // React 클라이언트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // JWT, 쿠키 사용 허용 (필요 시)
